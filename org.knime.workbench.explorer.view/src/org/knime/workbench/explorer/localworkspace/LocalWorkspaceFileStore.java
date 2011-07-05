77e613744c1894fb0ecff8803f7b9a1086ac78bc
Thorsten Meinl <thorsten.meinl@knime.com>
2011-07-05 08:56:14 +0000

    fixed copyright headers

--- org.knime.workbench.explorer.view/src/org/knime/workbench/explorer/localworkspace/LocalWorkspaceFileStore.java
+++ org.knime.workbench.explorer.view/src/org/knime/workbench/explorer/localworkspace/LocalWorkspaceFileStore.java
@@ -1,18 +1,48 @@
-/* ------------------------------------------------------------------
- * This source code, its documentation and all appendant files
- * are protected by copyright law. All rights reserved.
+/*
+ * ------------------------------------------------------------------------
  *
- * Copyright (c) KNIME.com, Zurich, Switzerland
+ *  Copyright (C) 2003 - 2011
+ *  University of Konstanz, Germany and
+ *  KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
  *
- * You may not modify, publish, transmit, transfer or sell, reproduce,
- * create derivative works from, distribute, perform, display, or in
- * any way exploit any of the content, in whole or in part, except as
- * otherwise expressly permitted in writing by the copyright owner or
- * as specified in the license file distributed with this product.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
  *
- * If you have any questions please contact the copyright holder:
- * website: www.knime.com
- * email: contact@knime.com
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
  * ---------------------------------------------------------------------
  *
  * Created: Apr 14, 2011
@@ -36,6 +66,7 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.knime.core.node.NodeLogger;
@@ -156,7 +187,7 @@ public class LocalWorkspaceFileStore extends ExplorerFileStore {
         }
         File targetDir = new File(dstFile, srcFile.getName());
         if (targetDir.exists()) {
-            throw new CoreException(new Status(Status.ERROR,
+            throw new CoreException(new Status(IStatus.ERROR,
                     ExplorerActivator.PLUGIN_ID, "A resource with the name "
                             + srcFile.getName() + " already exists in "
                             + dstFile.getName()));
@@ -171,7 +202,7 @@ public class LocalWorkspaceFileStore extends ExplorerFileStore {
             String message =
                     "Could not copy \"" + srcFile.getAbsolutePath()
                             + "\" to \"" + dstFile.getAbsolutePath() + "\".";
-            throw new CoreException(new Status(Status.ERROR,
+            throw new CoreException(new Status(IStatus.ERROR,
                     ExplorerActivator.PLUGIN_ID, message, e));
         }
         createProjectFile(destination, monitor);
@@ -220,7 +251,7 @@ public class LocalWorkspaceFileStore extends ExplorerFileStore {
                 String message =
                         "Could not create KNIME project in "
                                 + "workspace root.";
-                throw new CoreException(new Status(Status.ERROR,
+                throw new CoreException(new Status(IStatus.ERROR,
                         ExplorerActivator.PLUGIN_ID, message, e));
             }
         }
@@ -291,7 +322,7 @@ public class LocalWorkspaceFileStore extends ExplorerFileStore {
         } catch (IOException e) {
             String message =
                     "Could not delete \"" + srcFile.getAbsolutePath() + "\".";
-            throw new CoreException(new Status(Status.ERROR,
+            throw new CoreException(new Status(IStatus.ERROR,
                     ExplorerActivator.PLUGIN_ID, message, e));
         }
         refreshResource(getParent(), monitor);
@@ -334,7 +365,7 @@ public class LocalWorkspaceFileStore extends ExplorerFileStore {
                 + srcFile.getAbsolutePath() + "\" to \""
                 + dstFile.getAbsolutePath()
                 + "\" due to missing access rights.";
-            throw new CoreException(new Status(Status.ERROR,
+            throw new CoreException(new Status(IStatus.ERROR,
                     ExplorerActivator.PLUGIN_ID, message, e));
         }
         createProjectFile(destination.getName(), destination.getParent(),
