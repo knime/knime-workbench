a04e9e4d798fc1de9a0f64abe2ab68c9d9625d86
Thorsten Meinl <thorsten.meinl@knime.com>
2014-05-24 21:18:42 +0000

    Change copyright notices

--- org.knime.workbench.explorer.view/src/org/knime/workbench/explorer/view/actions/AbstractCopyMoveAction.java
+++ org.knime.workbench.explorer.view/src/org/knime/workbench/explorer/view/actions/AbstractCopyMoveAction.java
@@ -1,78 +1,46 @@
-/* ------------------------------------------------------------------
-  * This source code, its documentation and all appendant files
-  * are protected by copyright law. All rights reserved.
-  *
-  * Copyright by
-  * KNIME.com, Zurich, Switzerland
-  *
-  * You may not modify, publish, transmit, transfer or sell, reproduce,
-  * create derivative works from, distribute, perform, display, or in
-  * any way exploit any of the content, in whole or in part, except as
-  * otherwise expressly permitted in writing by the copyright owner or
-  * as specified in the license file distributed with this product.
-  *
-  * If you have any questions please contact the copyright holder:
-  * website: www.knime.com
-  * email: contact@knime.com
-  * ---------------------------------------------------------------------
-  *
-  * History
-  *   Oct 31, 2011 (morent): created
-  */
-
-package org.knime.workbench.explorer.view.actions;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.eclipse.core.filesystem.EFS;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.MultiStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.dialogs.ErrorDialog;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.jface.window.Window;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.ui.PlatformUI;
-import org.knime.core.node.NodeLogger;
-import org.knime.workbench.explorer.ExplorerActivator;
-import org.knime.workbench.explorer.ExplorerMountTable;
-import org.knime.workbench.explorer.dialogs.SpaceResourceSelectionDialog;
-import org.knime.workbench.explorer.dialogs.Validator;
-import org.knime.workbench.explorer.filesystem.AbstractExplorerFileInfo;
-import org.knime.workbench.explorer.filesystem.AbstractExplorerFileStore;
-import org.knime.workbench.explorer.filesystem.ExplorerFileSystemUtils;
-import org.knime.workbench.explorer.filesystem.LocalExplorerFileStore;
-import org.knime.workbench.explorer.filesystem.MessageFileStore;
-import org.knime.workbench.explorer.filesystem.RemoteExplorerFileStore;
-import org.knime.workbench.explorer.view.AbstractContentProvider;
-import org.knime.workbench.explorer.view.ContentDelegator;
-import org.knime.workbench.explorer.view.ContentObject;
-import org.knime.workbench.explorer.view.DestinationChecker;
-import org.knime.workbench.explorer.view.ExplorerView;
-import org.knime.workbench.explorer.view.dialogs.OverwriteAndMergeInfo;
-
-/**
- * Abstract base class for copy and move actions for the Explorer. It contains
- * basically all necessary functionality. Derived classes only have to
- * override the name and set the move flag.
- * @author Dominik Morent, KNIME.com, Zurich, Switzerland
+/*
+ * ------------------------------------------------------------------------
+ *  Copyright by KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
  *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ------------------------------------------------------------------------
  */
 public abstract class AbstractCopyMoveAction extends ExplorerAction {
     private static final NodeLogger LOGGER = NodeLogger.getLogger(
