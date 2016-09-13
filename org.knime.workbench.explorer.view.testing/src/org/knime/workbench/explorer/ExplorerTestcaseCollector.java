bf4eb9062076c6fa3957788616908ed3ab03edb2
Thorsten Meinl <thorsten.meinl@knime.com>
2016-09-13 13:10:18 +0000

    IN PROGRESS - issue SRV-324: Access to server space ignores permissions settings when read/write through knime:// protocol (e.g. mount point relative URLs) https://knime-com.atlassian.net/browse/SRV-324
    Added new testing fragment

---
+++ org.knime.workbench.explorer.view.testing/src/org/knime/workbench/explorer/ExplorerTestcaseCollector.java
@@ -0,0 +1,12 @@
+package org.knime.workbench.explorer;
+
+import org.knime.testing.core.AbstractTestcaseCollector;
+
+/**
+ * Testcase collector for this plug-in.
+ *
+ * @author Thorsten Meinl, KNIME.com, Zurich, Switzerland
+ */
+public class ExplorerTestcaseCollector extends AbstractTestcaseCollector {
+    // yes, it's empty
+}
