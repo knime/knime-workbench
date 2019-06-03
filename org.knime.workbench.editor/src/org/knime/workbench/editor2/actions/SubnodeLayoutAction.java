8fbe597d35e05a3bd48f51882305cefbc25fd130
Christian Albrecht <christian.albrecht@knime.com>
2019-06-03 13:59:52 +0200

    AP-11517: enable layout action also for pure dialog nodes
    AP-11517 (Split Quickforms into Dialog Nodes and Widgets)

--- org.knime.workbench.editor/src/org/knime/workbench/editor2/actions/SubnodeLayoutAction.java
+++ org.knime.workbench.editor/src/org/knime/workbench/editor2/actions/SubnodeLayoutAction.java
@@ -50,6 +50,7 @@ package org.knime.workbench.editor2.actions;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.knime.core.node.dialog.DialogNode;
 import org.knime.core.node.wizard.WizardNode;
 import org.knime.core.node.workflow.SubNodeContainer;
 import org.knime.core.node.workflow.WorkflowManager;
@@ -74,6 +75,10 @@ public class SubnodeLayoutAction extends AbstractNodeAction {
     /** filter for wizard node class */
     @SuppressWarnings("rawtypes")
     public static final NodeModelFilter<WizardNode> WIZARD_NODE_FILTER = new NodeModelFilter<WizardNode>();
+
+    /** filter for dialog node class */
+    @SuppressWarnings("rawtypes")
+    public static final NodeModelFilter<DialogNode> DIALOG_NODE_FILTER = new NodeModelFilter<DialogNode>();
 
     /**
      *
@@ -140,7 +145,9 @@ public class SubnodeLayoutAction extends AbstractNodeAction {
             return false;
         }
         if (manager.getDirectNCParent() instanceof SubNodeContainer) {
-            return !manager.findNodes(WizardNode.class, WIZARD_NODE_FILTER, false, true).isEmpty();
+            boolean containsViews = !manager.findNodes(WizardNode.class, WIZARD_NODE_FILTER, false, true).isEmpty();
+            boolean containsDialog = !manager.findNodes(DialogNode.class, DIALOG_NODE_FILTER, false, false).isEmpty();
+            return containsViews || containsDialog;
         }
         return false;
     }
