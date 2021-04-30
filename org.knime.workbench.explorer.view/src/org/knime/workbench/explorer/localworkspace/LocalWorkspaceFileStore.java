46a503595ef2b18131aaa4863eb9a9ba77d4bbf3
Martin Horn <martin.horn@knime.com>
2021-04-30 10:39:36 +0200

    AP-16655: Custom definition of mountpoint root for local file store
    The local mountpoint root is usually the eclipse workspace directory.
    However, if testflows are run with the TestflowRunnerApplication, those
    are executed outside of the eclipse workspace. This causes, e.g.,
    local, workflow-relative file/directory-accesses to fail (because the
    workspace was assumed to be the root directory).
    With this change, the TestflowRunnerApplication is able to set a custom
    mountpoint root directory.
    
    AP-16655 (TestflowRunner: fix workflow-relative reading/writing and enable component-download)

--- org.knime.workbench.explorer.view/src/org/knime/workbench/explorer/localworkspace/LocalWorkspaceFileStore.java
+++ org.knime.workbench.explorer.view/src/org/knime/workbench/explorer/localworkspace/LocalWorkspaceFileStore.java
@@ -54,6 +54,7 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.function.Supplier;
 
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
@@ -65,7 +66,6 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
-import org.knime.core.node.NodeLogger;
 import org.knime.core.util.PathUtils;
 import org.knime.workbench.explorer.ExplorerActivator;
 import org.knime.workbench.explorer.filesystem.AbstractExplorerFileStore;
@@ -81,7 +81,31 @@ import org.osgi.framework.FrameworkUtil;
  * @author ohl, University of Konstanz
  */
 public class LocalWorkspaceFileStore extends LocalExplorerFileStore {
-    private static final NodeLogger LOGGER = NodeLogger.getLogger(LocalExplorerFileStore.class);
+
+    private static Supplier<IPath> localMountpointRootSupplier = defaultLocalMountPointRootSupplier();
+
+    /**
+     * Overwrites the default mountpoint root supplier (i.e. a function that supplies the absolute root directory for
+     * all workflows). By default it's the eclipse workspace directory and usually doesn't need to be overwritten
+     * (mainly only used for testflow runs).
+     *
+     * @param supplier the supplier, never <code>null</code>, the supplier itself must also never return
+     *            <code>null</code>
+     */
+    public static void setLocalMountPointRootSupplier(final Supplier<IPath> supplier) {
+        localMountpointRootSupplier = supplier;
+    }
+
+    /**
+     * Resets the mountpoint root supplier back to the default one (i.e. the eclipse workspace directory).
+     */
+    public static void resetLocalMountPointRootSupplier() {
+        localMountpointRootSupplier = defaultLocalMountPointRootSupplier();
+    }
+
+    private static Supplier<IPath> defaultLocalMountPointRootSupplier() {
+        return () -> ResourcesPlugin.getWorkspace().getRoot().getLocation();
+    }
 
     private final IFileStore m_file;
 
@@ -91,7 +115,7 @@ public class LocalWorkspaceFileStore extends LocalExplorerFileStore {
      */
     public LocalWorkspaceFileStore(final String mountID, final String fullPath) {
         super(mountID, fullPath);
-        IPath rootPath = ResourcesPlugin.getWorkspace().getRoot().getLocation();
+        IPath rootPath = localMountpointRootSupplier.get();
         IPath filePath = rootPath.append(new Path(getFullName()));
         m_file = EFS.getLocalFileSystem().getStore(filePath);
     }
