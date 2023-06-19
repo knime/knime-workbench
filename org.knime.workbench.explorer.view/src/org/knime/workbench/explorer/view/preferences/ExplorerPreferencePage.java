f4876e806b4a264f93fdba350fdb6e7fb1ec3661
Kai Franze <kai.franze@knime.com>
2023-06-19 10:24:04 +0200

    NXT-1795: Small corrections after code review
    NXT-1795 (Backend changes to restructure AP preference dialog)

--- org.knime.workbench.explorer.view/src/org/knime/workbench/explorer/view/preferences/ExplorerPreferencePage.java
+++ org.knime.workbench.explorer.view/src/org/knime/workbench/explorer/view/preferences/ExplorerPreferencePage.java
@@ -74,7 +74,7 @@ public class ExplorerPreferencePage extends FieldEditorPreferencePage
     public ExplorerPreferencePage() {
         super("KNIME Explorer Settings", null, GRID);
         setDescription("Set up mount points for usage in KNIME Explorer view. "
-            + "In KNIME Modern UI only mount points of type \"KNIME Hub\" are shown in the explorer.");
+            + "In KNIME Modern UI only mount points of type “KNIME Hub” are shown in the explorer.");
     }
 
     /**
