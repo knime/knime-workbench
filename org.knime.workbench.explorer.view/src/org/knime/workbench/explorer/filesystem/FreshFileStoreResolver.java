91ddee5afb5128445714df1c1235af4f162fec4d
Manuel Hotz <manuel.hotz@knime.com>
2025-03-18 17:51:56 +0100

    AP-24117: Remove refresh logic from `FreshFileStoreResolver`
    ... so plugins can still find the class, but refresh behavior is effectively dead again (because repository fetchers
    keep remote content up to date again like in 5.3)
    
    AP-24117 (Partial AP update to 5.4.3 can lead to workflow save problem)

--- org.knime.workbench.explorer.view/src/org/knime/workbench/explorer/filesystem/FreshFileStoreResolver.java
+++ org.knime.workbench.explorer.view/src/org/knime/workbench/explorer/filesystem/FreshFileStoreResolver.java
@@ -45,23 +45,7 @@
  */
 package org.knime.workbench.explorer.filesystem;
 
-import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Collectors;
-
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.ui.PlatformUI;
-import org.knime.core.node.NodeLogger;
-import org.knime.workbench.explorer.ExplorerMountTable;
-import org.knime.workbench.explorer.view.AbstractContentProvider;
 
 /**
  * Resolve a KNIME URI to an {@link AbstractExplorerFileStore}. This is in principle not a good idea since the latter
@@ -75,6 +59,8 @@ import org.knime.workbench.explorer.view.AbstractContentProvider;
  *
  * @noreference This class is not intended to be referenced by clients.
  * @since 8.14
+ * @deprecated this resolver does not refresh remotes anymore, because the repository fetchers keep content up to date
+ *             again
  */
 @Deprecated(forRemoval = true)
 public final class FreshFileStoreResolver {
@@ -89,7 +75,7 @@ public final class FreshFileStoreResolver {
 
     /**
      * Refresh a single item.
-     * 
+     *
      * @param knimeURI The KNIME URI of the item.
      * @return
      */
@@ -98,22 +84,8 @@ public final class FreshFileStoreResolver {
         if (!(fileStore instanceof RemoteExplorerFileStore remoteFileStore)) {
             return fileStore; // not remote, nothing to refresh
         }
-        refreshWithProgress(List.of(remoteFileStore));
+        // no manual refresh necessary, repository fetchers keep everything up to date again
         return remoteFileStore;
-    }
-
-    private static List<RemoteExplorerFileStore> findContentProviders(final Set<String> mountIds) {
-        return ExplorerMountTable.getMountedContent() //
-            .values().stream() //
-            // filter mounted with current space providers, to not refresh remotes that are not visible in
-            // ModernUI, if any
-            .filter(contentProvider -> mountIds.contains(contentProvider.getMountID())) //
-            // we don't need to refresh local and currently unconnected
-            .filter(contentProvider -> contentProvider.isRemote() && contentProvider.isAuthenticated()) //
-            .map(AbstractContentProvider::getRootStore) //
-            .filter(RemoteExplorerFileStore.class::isInstance) //)
-            .map(RemoteExplorerFileStore.class::cast) //)
-            .toList();
     }
 
     /**
@@ -122,95 +94,7 @@ public final class FreshFileStoreResolver {
      * cancelled.
      */
     public static void refreshContentProvidersWithProgress(final String... mountIds) {
-        var fileStores = findContentProviders(Arrays.stream(mountIds).collect(Collectors.toUnmodifiableSet()));
-        if (fileStores.isEmpty()) {
-            return;
-        }
-        refreshWithProgress(fileStores);
-    }
-
-    private static void refreshWithProgress(final List<RemoteExplorerFileStore> fileStores) {
-        PlatformUI.getWorkbench().getDisplay().syncCall(() -> { // NOSONAR
-            fileStores.forEach(store -> new RefreshJob(store).schedule());
-            return joinOnJobFamily(RefreshJob.class);
-        });
-
-    }
-
-    /**
-     * Wait for jobs belonging to {@code family} to finish. In contrast to `new RefreshJob(...).run(monitor)`, this lets
-     * us cancel individual jobs or the whole thing. For some reason, the former does not cancel jobs on "Cancel".
-     *
-     * @param family see {@link Job#belongsTo(Object)} and
-     *            {@link org.eclipse.core.runtime.jobs.IJobManager#join(Object, IProgressMonitor)}
-     * @return Whether the job family execution has been cancelled.
-     */
-    private static boolean joinOnJobFamily(final Object family) {
-        final var canceled = new AtomicBoolean(false);
-        try {
-            PlatformUI.getWorkbench().getProgressService().busyCursorWhile(monitor -> { // NOSONAR complexity ok
-                try {
-                    // We wait for our refresh jobs to finish.
-                    Job.getJobManager().join(family, monitor);
-                } catch (final OperationCanceledException e) { // NOSONAR we handle this exception
-                    // user hit cancel, suggest jobs to cancel as well. Unfortunately, it's not possible to
-                    // find the job's thread to interrupt it... :(
-                    for (final var job : Job.getJobManager().find(family)) {
-                        job.cancel();
-                    }
-                    // handle refresh cancellation as complete cancellation, i.e. not opening the picker, too
-                    canceled.set(true);
-                    return;
-                }
-            });
-        } catch (final InvocationTargetException e) {
-            NodeLogger.getLogger(FreshFileStoreResolver.class)
-                .warn("Failed to refresh remote content before opening dialog – content might be stale.", e);
-        } catch (final InterruptedException e) { // NOSONAR we recover from that
-            // we got interrupted while waiting for refresh, so we cancel the whole thing
-            canceled.set(true);
-        }
-        return canceled.get();
-    }
-
-    /**
-     * Eclipse workbench job to refresh a given {@link RemoteExplorerFileStore}.
-     */
-    private static final class RefreshJob extends Job {
-
-        private final RemoteExplorerFileStore m_store;
-
-        private RefreshJob(final RemoteExplorerFileStore store) {
-            super("Refresh remote content");
-            setUser(true);
-            m_store = store;
-        }
-
-        @Override
-        public boolean belongsTo(final Object family) {
-            return family == RefreshJob.class;
-        }
-
-        @Override
-        protected IStatus run(final IProgressMonitor monitor) {
-            monitor.beginTask("Refreshing \"%s\"...".formatted(m_store.getMountID()), IProgressMonitor.UNKNOWN);
-            if (monitor.isCanceled()) {
-                return Status.CANCEL_STATUS;
-            }
-            m_store.refresh(true);
-            monitor.done();
-            return Status.OK_STATUS;
-
-        }
-
-        @Override
-        protected void canceling() {
-            super.canceling();
-            // we need to interrupt the thread, because the store refresh does not check the progress monitor for
-            // cancellation
-            getThread().interrupt();
-        }
-
+        // no manual refresh necessary, repository fetchers keep everything up to date again
     }
 
 }
